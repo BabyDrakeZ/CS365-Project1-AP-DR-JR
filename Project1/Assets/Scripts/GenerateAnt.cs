17b0b82f83a01fe66e73c557a2f7551fc72ae514
David Richman <david.c.richman@gmail.com>
2022-10-30 14:35:48 -0400

    Splash manager with constants

--- Project1/Assets/Scripts/GenerateAnt.cs
+++ Project1/Assets/Scripts/GenerateAnt.cs
@@ -8,6 +8,7 @@ public class GenerateAnt : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Constants.C.food = 50;
     }
 
     // Update is called once per frame
