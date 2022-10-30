9a4ff902b388bae690ae613dbbd5df22334ca79a
David Richman <david.c.richman@gmail.com>
2022-10-30 15:10:56 -0400

    Debugging

--- Project1/Assets/Scripts/Swarm/GenerateAnt.cs
+++ Project1/Assets/Scripts/Swarm/GenerateAnt.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GenerateAnt : MonoBehaviour
 {
-    public int foodCost;
+    public int foodCost = 10;
     public GameObject swarm;
     public GameObject spawnPoint;
     // Start is called before the first frame update
