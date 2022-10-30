40a0ef0397e586f1838a22e6109f69b6775d40ea
David Richman <david.c.richman@gmail.com>
2022-10-30 14:29:30 -0400

    Adding constants and prefabs

---
+++ Project1/Assets/Scripts/Constants.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants : MonoBehaviour
+{
+    static public Constants C;
+    public int food;
+    // Start is called before the first frame update
+    void Start()
+    {
+        C = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
